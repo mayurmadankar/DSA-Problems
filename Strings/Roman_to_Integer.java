94dca76fd6c7a00400a45c3eced70406806280fe
Mayur <madankarmayur5@gmail.com>
2023-08-09 10:41:59 +0530

    adding valid parenthesis file

---
+++ Strings/Roman_to_Integer.java
@@ -0,0 +1,45 @@
+class Solution {
+    public int romanToInt(String s) {
+        int result =0;
+        int prev =0;
+
+        for ( int i=s.length()-1 ; i>=0 ; i--){
+            int current = getValue(s.charAt(i));
+
+            if(current < prev ){
+                result -= current;
+            }else{
+                result += current;
+            }
+            prev = current;
+        }
+        return result;
+    }
+    public static int getValue(char c){
+        switch(c){
+            case 'I' : 
+            return 1;
+
+            case 'V' :
+            return 5;
+
+            case 'X' :
+            return 10;
+
+            case 'L' :
+            return 50;
+
+            case 'C' :
+            return 100;
+
+            case 'D' :
+            return 500;
+
+            case 'M' :
+            return 1000;
+
+            default :
+            return 0; 
+        }
+    }
+}
