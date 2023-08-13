e55e6274036b9ada496787184e45bf77d2e095d0
Mayur <madankarmayur5@gmail.com>
2023-08-13 19:12:15 +0530

    adding reverse words in string file

---
+++ Strings/Reverse_Words_in_a_String.java
@@ -0,0 +1,14 @@
+//time complexity - O(n)
+//space complexity -O(1)
+class Solution {
+    public String reverseWords(String s) {
+       String[] word = s.split(" +");
+       StringBuilder sb = new StringBuilder();
+
+       for(int i= word.length-1 ; i>=0 ;i--){
+           sb.append(word[i]);
+           sb.append(" ");
+       }
+       return sb.toString().trim();
+    }
+}
