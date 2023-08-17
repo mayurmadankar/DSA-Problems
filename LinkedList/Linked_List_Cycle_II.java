f4a3ed6ac750febae270282eda341512302694e2
Mayur <madankarmayur5@gmail.com>
2023-08-17 15:15:38 +0530

    adding linked list cycle II file

---
+++ LinkedList/Linked_List_Cycle_II.java
@@ -0,0 +1,47 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+//  time complexity -O(n)
+// space complexity -O(1)
+
+public class Solution {
+    public ListNode detectCycle(ListNode head) {
+        if (head == null || head.next == null) {
+            return null;
+        }
+        
+        ListNode slow = head;
+        ListNode fast = head;
+        boolean hasCycle = false;
+
+        while (fast.next != null && fast.next.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            
+            if (slow == fast) {
+                hasCycle = true;
+                break;
+            }
+        }
+        
+        if (!hasCycle) {
+            return null;
+        }
+        
+        ListNode entry = head;
+        while (entry != slow) {
+            entry = entry.next;
+            slow = slow.next;
+        }
+        
+        return entry;
+    }
+}
