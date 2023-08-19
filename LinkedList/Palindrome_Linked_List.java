a4f57d565c825296241d4313417d44845d67e3f2
Mayur <madankarmayur5@gmail.com>
2023-08-19 19:28:16 +0530

    adding sort list file

---
+++ LinkedList/Palindrome_Linked_List.java
@@ -0,0 +1,51 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+
+class Solution {
+    public boolean isPalindrome(ListNode head) {
+        if(head ==null || head.next==null){
+            return true;
+        }
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while(fast.next!=null && fast.next.next!=null){
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        ListNode secondHalf = reverse(slow.next);
+
+        ListNode p1=head;
+        ListNode p2=secondHalf;
+
+        while(p2!=null){
+            if(p1.val!=p2.val){
+                return false;
+            }
+                p1=p1.next;
+                p2=p2.next;
+        }        
+       return true;
+  }
+   private static ListNode reverse(ListNode head){
+            ListNode curr=head;
+            ListNode prev=null;
+
+            while(curr!=null){
+                ListNode next = curr.next;
+                curr.next=prev;
+                prev=curr;
+                curr=next;
+            }
+            return prev;
+        }
+}
---
+++ LinkedList/Sort_List.java
@@ -0,0 +1,61 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode sortList(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+
+        ListNode mid = findMiddle(head);
+        ListNode left = head;
+        ListNode right = mid.next;
+        mid.next = null;
+
+        ListNode sortedLeft = sortList(left);
+        ListNode sortedRight = sortList(right);
+
+        return merge(sortedLeft, sortedRight);
+    }
+
+    private ListNode findMiddle(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+        while (fast.next != null && fast.next.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow;
+    }
+
+    private ListNode merge(ListNode left, ListNode right) {
+        ListNode dummy = new ListNode(0);
+        ListNode current = dummy;
+
+        while (left != null && right != null) {
+            if (left.val < right.val) {
+                current.next = left;
+                left = left.next;
+            } else {
+                current.next = right;
+                right = right.next;
+            }
+            current = current.next;
+        }
+
+        if (left != null) {
+            current.next = left;
+        } else {
+            current.next = right;
+        }
+
+        return dummy.next;
+    }
+}
