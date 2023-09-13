2b0b0e5e342111981f2efc0ff598cc8816d98ce0
Mayur <madankarmayur5@gmail.com>
2023-09-13 19:02:38 +0530

    adding post order traversal

---
+++ Tree/PostOrder_Traversal.java
@@ -0,0 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<Integer> postorderTraversal(TreeNode root) {
+        
+        List<Integer> result = new ArrayList<>();
+        order(root , result);
+        return result;
+    }
+    private static void order(TreeNode root , List<Integer> result){
+        if(root==null){
+            return;
+        }
+        order(root.left,result);
+        order(root.right , result);
+        result.add(root.val);
+    }
+}
