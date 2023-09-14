9d44cd1ab3b3e015d232a2c2b9554020ed00c422
Mayur <madankarmayur5@gmail.com>
2023-09-14 14:27:11 +0530

    adding inorder Traversal iterative code

--- Tree/InOrder_Traversal.java
+++ Tree/InOrder_Traversal.java
@@ -13,19 +13,46 @@
  *     }
  * }
  */
+
+//  Recursive Way :-
+
+// class Solution {
+//     public List<Integer> inorderTraversal(TreeNode root) {
+//        List<Integer> result = new ArrayList<>();
+//        inOrderHelper(root , result);
+//        return result;
+//     }
+//     private static void inOrderHelper(TreeNode node , List<Integer> result){
+//         if(node == null){
+//             return;
+//         }
+//         inOrderHelper(node.left , result);
+//         result.add(node.val);
+//         inOrderHelper(node.right , result);
+        
+//     }
+// }
+
+// Iterative way :-
+
 class Solution {
     public List<Integer> inorderTraversal(TreeNode root) {
-       List<Integer> result = new ArrayList<>();
-       inOrderHelper(root , result);
-       return result;
-    }
-    private static void inOrderHelper(TreeNode node , List<Integer> result){
-        if(node == null){
-            return;
+        Stack<TreeNode> st = new Stack<TreeNode>();
+        List<Integer> inorder = new LinkedList<Integer>();
+        TreeNode node = root;
+        while(true){
+            if(node!=null){
+                st.push(node);
+                node = node.left;
+            }else{
+                if(st.isEmpty()){
+                    break;
+                }
+                node=st.pop();
+                inorder.add(node.val);
+                node = node.right;
+            }
         }
-        inOrderHelper(node.left , result);
-        result.add(node.val);
-        inOrderHelper(node.right , result);
-        
+        return inorder;
     }
 }
