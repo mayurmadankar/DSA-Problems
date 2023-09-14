037a4853b6a7c5b90069098fa6d7ab2df2df6654
Mayur <madankarmayur5@gmail.com>
2023-09-14 13:40:58 +0530

    adding edited code of preorder traversal

--- Tree/PreOrder_Traversal.java
+++ Tree/PreOrder_Traversal.java
@@ -13,18 +13,45 @@
  *     }
  * }
  */
+//  Recursive way:-
+
+// class Solution {
+//     public List<Integer> preorderTraversal(TreeNode root) {
+//         List<Integer> result = new ArrayList<>();
+//         pre(root , result);
+//         return result;
+//     }
+//     private static void pre(TreeNode root , List<Integer> result){
+//         if(root==null){
+//             return;
+//         }
+//         result.add(root.val);
+//         pre(root.left , result);
+//         pre(root.right , result);
+//     }
+// }
+
+// Iterative way :-
+
 class Solution {
-    public List<Integer> preorderTraversal(TreeNode root) {
-        List<Integer> result = new ArrayList<>();
-        pre(root , result);
-        return result;
-    }
-    private static void pre(TreeNode root , List<Integer> result){
+    public List<Integer> preorderTraversal (TreeNode root){
+        List<Integer> preorder = new LinkedList<>();
+        Stack<TreeNode> st = new Stack<TreeNode>();
         if(root==null){
-            return;
+            return preorder;
         }
-        result.add(root.val);
-        pre(root.left , result);
-        pre(root.right , result);
+        st.push(root);
+        while(!st.isEmpty()){
+            root=st.pop();
+            preorder.add(root.val);
+            
+            if(root.right!=null){
+                st.add(root.right);
+            }
+            if(root.left!=null){
+                st.add(root.left);
+            }
+        }
+        return preorder;      
     }
 }
