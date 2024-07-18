c63f9347a20f86e6baaf777d94b931c37814dda2
Mayur <madankarmayur5@gmail.com>
2024-07-18 14:31:30 +0530

    XOR Property

---
+++ Strings/Single_Number.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int singleNumber(int[] nums) {
+        int result=0;
+        for(int i=0;i<nums.length;i++){
+            result^=nums[i];
+        }
+        return result;
+    }
+}
+
+// Intuition:
+// Two important properties of XOR are the following:
+
+// XOR of two same numbers is always 0 i.e. a ^ a = 0. ←Property 1.
+// XOR of a number with 0 will result in the number itself i.e. 0 ^ a = a.  ←Property 2
+
+// Assume the given array is: [4,1,2,1,2]
+// XOR of all elements = 4^1^2^1^2
+//       = 4 ^ (1^1) ^ (2^2)
+//       = 4 ^ 0 ^ 0 = 4^0 = 4
+// Hence, 4 is the single element in the array.
+
+// So, if we perform the XOR of all the numbers of the array elements, we will be left with a single number.
+
+// tc-O(n)
+// sc-O(1)
