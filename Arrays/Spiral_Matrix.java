53771360e51224dc90d412edfd69019dcd7284cc
Mayur <madankarmayur5@gmail.com>
2024-07-20 00:55:20 +0530

    20 july 2024

---
+++ Arrays/Spiral_Matrix.java
@@ -0,0 +1,37 @@
+class Solution {
+    public List<Integer> spiralOrder(int[][] matrix) {
+        int n=matrix.length;
+        int m=matrix[0].length;
+
+        int left=0;
+        int right=m-1;
+        int top=0;
+        int bottom=n-1;
+
+        List<Integer> result = new ArrayList<>();
+
+        while(left<=right && top<=bottom){
+            for(int i=left;i<=right;i++){
+                result.add(matrix[top][i]);
+            }
+            top++;
+            for(int i=top;i<=bottom;i++){
+                result.add(matrix[i][right]);
+            }
+            right--;
+            if(top<=bottom){
+                for(int i=right;i>=left;i--){
+                    result.add(matrix[bottom][i]);
+                }
+                bottom--;
+            }
+            if(left<=right){
+                for(int i=bottom;i>=top;i--){
+                    result.add(matrix[i][left]);
+                }
+                left++;
+            }
+        }
+        return result;
+    }
+}
