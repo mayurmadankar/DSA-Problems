3d58378bcbbe4c3ad225ebfc1e8cf5c3265f7d1e
Mayur <madankarmayur5@gmail.com>
2024-09-26 12:16:47 +0530

    easy

---
+++ Arrays/Best_Time_to_Buy_Stacks_II.java
@@ -0,0 +1,14 @@
+//tc-O(n)
+//sc-O(1)
+class Solution {
+    public int maxProfit(int[] prices) {
+        int totalProfit=0;
+
+        for(int i=1;i<prices.length;i++){
+            if(prices[i-1]<prices[i]){
+                totalProfit+=prices[i]-prices[i-1];
+            }
+        }
+        return totalProfit;
+    }
+}
